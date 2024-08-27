6ea465f16035a1bbf11392c05dd72a2093c15aef
Renatog2 <r.g.moraes@hotmail.com>
2024-08-27 13:55:44 -0300

    Cap 15 Finished

---
+++ course15_exerc1/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		try {
+			System.out.println("Enter account data:");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			Account account = new Account(number, name, balance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double withdraw = sc.nextDouble();
+			account.withdraw(withdraw);
+			
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+		} catch (DomainException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (InputMismatchException e) {
+			System.out.println("Incorrect input");
+		}catch (RuntimeException e) {
+			System.out.println("Unexpected error: " + e.getMessage());
+		}
+		
+		
+		sc.close();
+	}
+}
